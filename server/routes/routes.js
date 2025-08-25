10b7f7a24ebf318825d25cc3c550c5317ddb2b92
NSZ <nszgames4@gmail.com>
2025-08-25 09:10:54 +0100

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -18,4 +18,26 @@ module.exports = app => {
             color: 'black'
         });
     });
+
+    // Result page for end of game
+    app.get('/result', (req, res) => {
+        const outcome = (req.query.outcome || '').toLowerCase(); // 'win' | 'lose' | 'stalemate'
+        let title = 'Game Over';
+        let message = '';
+
+        if (outcome === 'win') {
+            title = 'You Win!';
+            message = 'Great game. Well played!';
+        } else if (outcome === 'lose') {
+            title = 'You Lose';
+            message = 'Good effort. Try again!';
+        } else if (outcome === 'stalemate') {
+            title = 'Stalemate';
+            message = 'The game ended in a stalemate.';
+        } else {
+            return res.redirect('/');
+        }
+
+        res.render('result', { title, message, outcome });
+    });
 };
